a70a6d5a05666ee21654b3ea0896c9ebb3cec78a
Edumc <eduardomar987@gmail.com>
2022-07-21 21:50:43 -0500

    Agrege Dinamicamente las peliculas y borre los div

--- app.js
+++ app.js
@@ -3,13 +3,33 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_URL = BASE_URL + '/movie/popular?api_key=' + API_KEY;
 
+const API_IMAGEN = 'https://image.tmdb.org/t/p/original/'
+
+const containerCarrusel = document.querySelector('.carrusel')
+
+
 
 function getMovies(url){
     fetch(url).then(response => response.json()).then(data => {
-        console.log(data);
+        console.log(data.results);
+        paintMovies(data.results)
     }
     )
 }
 
-let datos = getMovies(API_URL);
+getMovies(API_URL);
 
+    
+
+function paintMovies(data){
+    
+    data.forEach(movie => {
+        const {title, poster_path} = movie
+    let divMovie = document.createElement('div')
+    divMovie.classList.add('box-pelis')
+    divMovie.innerHTML = `
+    <a href="#"><img src="${API_IMAGEN}${poster_path}" alt="${title}"></a>
+    `
+    containerCarrusel.appendChild(divMovie)
+    });
+}
